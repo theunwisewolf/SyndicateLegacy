ffea3e4b7726b1a15f556dd0c5b2b73eb3d7dec1
srmu <askamnusa@gmail.com>
2017-02-17 15:04:49 +0530

    Added Font Class

--- Core/Systems/Graphics/Layers/Label.h
+++ Core/Systems/Graphics/Layers/Label.h
@@ -1,10 +1,16 @@
 #ifndef LABEL_H_
 #define LABEL_H_
 
+#include <string>
+
 namespace Venus { namespace Graphics { 
 
 class Label {
 
+private:
+
+public:
+    void drawString(std::string text, float x, float y, Font font);
 };
 
 }}
