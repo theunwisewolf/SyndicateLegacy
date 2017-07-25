9787fb4f03fcd99b9a9cce92730fb3ba86acd6b5
AmN <askamnusa@gmail.com>
2017-07-25 12:12:53 +0530

    Added missing libs to version control. Fixed OpenAL dynamic lib for x64 lib.

--- SyndicateCore/Dependencies/Box2D/modified/Common.h
+++
@@ -1,7 +0,0 @@
-#ifdef SYNDICATE_DYNAMIC
-#	define SYNDICATE_API		__declspec(dllexport)
-#	define SYNDICATE_TEMPLATE
-#else
-#	define SYNDICATE_API		__declspec(dllimport)
-#	define SYNDICATE_TEMPLATE   extern
-#endif
